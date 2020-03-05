05edbc7dbffe9888c8a30c2583bb42ea11872544
CapUlDis <capuldis@gmail.com>
2020-03-05 15:22:25 +0300

    КТО? в лс работает.

--- main.js
+++ main.js
@@ -1,37 +1,26 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const {Botact} = require('node-vk-bot-api')
+const express = require('express');
+const bodyParser = require('body-parser');
+const VKbot = require('node-vk-bot-api/lib');
 
-const app = express()
-const bot = new Botact({
+const app = express();
+const bot = new VKbot({
     token: 'd4b54aac31841a8c5e570bd193bf624fc50fda63fe890d83041d5491b70d64d84b50b974de9a245c116c5',
     confirmation: '7d990e40'
-})
+});
 
-bot.on(function (ctx) {
-    console.log(ctx.body)
+// bot.on((ctx) => {
+//     console.log(ctx.body);
+//     ctx.reply('assert');
+// });
 
-    ctx.reply(ctx.body)
-})
+bot.command('бот?', (ctx) => {
+    ctx.reply('КТО?!');
+});
 
-bot.command('start', function (ctx) {
-    ctx.reply('Вы успешно стартовали!')
-})
+bot.startPolling();
 
-bot.command('время', function (ctx) {
-    const date = new Date()
+app.use(bodyParser.json());
 
-    const h = date.getHours()
-    const m = date.getMinutes()
-    const s = date.getSeconds()
+app.post('/', bot.webhookCallback);
 
-    const time = 'Сейчас ' + h + ':' + m + ':' + s
-
-    ctx.reply(time)
-})
-
-app.use(bodyParser.json())
-
-app.post('/', bot.listen)
-
-app.listen(80)
+app.listen(80);
