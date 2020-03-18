18856d5fa0d894369662cfe1850e538fcc803116
CapUlDis <capuldis@gmail.com>
2020-03-18 17:30:25 +0300

    work in progress.

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,31 +1,78 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const creds = require('./client_secret.json');
 
-class Sheet {
-    constructor({ sheetId }) {
-        this.sheetId = sheetId;
-        return this.getRows();
-    }
+// class Sheet {
+//     constructor({ sheetId }) {
+//         this.sheetId = sheetId;
+//         return this.getRows();
+//     }
 
-    async init() {
-        const rows = await this.getRows();
-        return rows;
-    }
+//     async init() {
+//         const rows = await this.getRows();
+//         return rows;
+//     }
 
-    async getRows() {
-        const doc = new GoogleSpreadsheet(this.sheetId);
-        await doc.useServiceAccountAuth(creds);
-        await doc.loadInfo();
-        const sheet = doc.sheetsByIndex[0];
-        const rows = await sheet.getRows();
-        return rows;
-    }
+//     async getRows() {
+//         const doc = new GoogleSpreadsheet(this.sheetId);
+//         await doc.useServiceAccountAuth(creds);
+//         await doc.loadInfo();
+//         const sheet = doc.sheetsByIndex[0];
+//         const rows = await sheet.getRows();
+//         return rows;
+//     }
+// }
+
+// // const sheet = new Sheet({ sheetId: '1_Wah-_jNWevQMYtXKCv9Zog3isPT2xzxzdkgm-0qLUo' }).then((rows) => {
+// //     console.log(rows)
+// // });
+
+// // console.log(sheet);
+
+// class Sheettest {
+//     constructor({ sheetId }) {
+//         this.sheetId = sheetId;
+//         this.doc = new GoogleSpreadsheet(this.sheetId);
+//         this.sheet = null;
+//         this.rows = null;
+//     }
+
+//     async init() {
+//         await this.doc.useServiceAccountAuth(creds);
+//         await this.doc.loadInfo();
+//         this.sheet = this.doc.sheetsByIndex[0];
+//         this.rows = await this.sheet.getRows();
+//         return this
+//     }
+// }
+
+// const sheet = new Sheettest({ sheetId: '1_Wah-_jNWevQMYtXKCv9Zog3isPT2xzxzdkgm-0qLUo' }).init().then((sheet) => {
+//     console.log(Sheettest.sheet.rowCount)
+// });
+
+
+// async function access() {
+//     const doc = new GoogleSpreadsheet('1_Wah-_jNWevQMYtXKCv9Zog3isPT2xzxzdkgm-0qLUo');
+//     await doc.useServiceAccountAuth(creds);
+//     await doc.loadInfo();
+//     const sheet = doc.sheetsByIndex[0];
+//     const rows = await sheet.getRows();
+//     console.log(`Title: ${doc.title}, Rows: ${sheet.rowCount}`);
+//     console.log(sheet.headerValues);
+//     for (i = 0; i <= 7; i++) {
+//         console.log(`${rows[i].Период} ${rows[i].Кухня} ${rows[i].КВТ}`);
+//     };
+// }
+
+
+const duty_table = new GoogleSpreadsheet('1_Wah-_jNWevQMYtXKCv9Zog3isPT2xzxzdkgm-0qLUo');
+const dt = null;
+
+async function getDoc(doc) {
+    await doc.useServiceAccountAuth(creds);
+    await doc.loadInfo();
+    return doc;
 }
 
-const sheet = new Sheet({ sheetId: '1_Wah-_jNWevQMYtXKCv9Zog3isPT2xzxzdkgm-0qLUo' }).then((rows) => {
-    console.log(rows)
-});
-
-console.log(sheet);
-
+getDoc(duty_table).then();
+console.log(duty_table);
 
