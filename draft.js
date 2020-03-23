ba0fc233a8cb166ab811f89671f523972f115cdb
CapUlDis <capuldis@gmail.com>
2020-03-23 12:38:03 +0300

    Add .gitignore.

---
+++ draft.js
@@ -0,0 +1,20 @@
+// const Spreadsheet = require('./spreadsheet')
+
+
+// async function testClass() {
+//   const sprdsheet = new Spreadsheet('1_Wah-_jNWevQMYtXKCv9Zog3isPT2xzxzdkgm-0qLUo');
+//   console.log(`Title: ${sprdsheet.doc.title}, Rows: ${sprdsheet.sheet.rowCount}`);
+//   console.log(sprdsheet.sheet.headerValues);
+//     for (i = 0; i <= 7; i++) {
+//         console.log(`${sprdsheet.rows[i].Период} ${sprdsheet.rows[i].Кухня} ${sprdsheet.rows[i].КВТ}`);
+//     };
+// }
+
+// testClass();
+
+
+// function resolveAfter2Seconds(x) {
+//   return new Promise(resolve => { setTimeout(() => {resolve(x);}, 2000);
+//   });
+// }
+
