d2c97a390baac4c69d4babe3bcc0879e385e9de8
CapUlDis <capuldis@gmail.com>
2020-03-23 13:21:46 +0300

    Set env vars with dotenv package for VK credits and SheetId.

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const VKbot = require('node-vk-bot-api/lib');
 const { GoogleTable } = require('./spreadsheet');
+require('dotenv').config()
 
 
 const app = express();
 const bot = new VKbot({
-    token: 'd4b54aac31841a8c5e570bd193bf624fc50fda63fe890d83041d5491b70d64d84b50b974de9a245c116c5',
-    confirmation: '7d990e40'
+    token: process.env.VK_TOKEN,
+    confirmation: process.env.VK_CONFIRM
 });
-const table_duty = new GoogleTable('1_Wah-_jNWevQMYtXKCv9Zog3isPT2xzxzdkgm-0qLUo');
+const table_duty = new GoogleTable(process.env.SPREADSHEET_ID);
 
 
 
