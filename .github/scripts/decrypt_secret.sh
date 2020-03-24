081345c2c4e6a609fe28b153cf3cf780c511276f
CapUlDis <capuldis@gmail.com>
2020-03-24 11:18:11 +0300

    Fix some nits.

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
---output $HOME/secrets/client_secret.json client_secret.json.gpg
+--output $HOME/client_secret.json client_secret.json.gpg
