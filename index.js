213b9186087c2bdf354ceae65b4b8177a5482bd9
CapUlDis <capuldis@gmail.com>
2020-03-26 10:54:16 +0300

    Add expressPino to logger in index.js

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const VKbot = require('node-vk-bot-api/lib');
 const PORT = process.env.PORT || 3000
 const { GoogleTable } = require('./spreadsheet');
 const pino = require('pino');
+const expressPino = require('express-pino-logger');
 
 const logger = pino({ level: process.env.LOV_LEVEL || 'info'});
+const expressLogger = expressPino({ logger });
 
 const app = express();
 const bot = new VKbot({
@@ -35,6 +37,8 @@ bot.command('дежурство', (ctx) => {
 bot.command('тест1', (ctx) => {
     ctx.reply('Ребята не стоит вскрывать эту тему. Вы молодые, шутливые, вам все легко. Это не то. Это не Чикатило и даже не архивы спецслужб. Сюда лучше не лезть. Серьезно, любой из вас будет жалеть. Лучше закройте тему и забудьте что тут писалось. Я вполне понимаю что данным сообщением вызову дополнительный интерес, но хочу сразу предостеречь пытливых - стоп. Остальные просто не найдут.');
 });
+
+app.use(expressLogger);
 
 app.use(bodyParser.json());
 
