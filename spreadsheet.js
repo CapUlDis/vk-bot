a5b678d0b18658101bb65e3581aba6610fffcf40
CapUlDis <capuldis@gmail.com>
2020-03-26 15:08:08 +0300

    Add some instruction to missing connection of Google Spreadsheet.

--- spreadsheet.js
+++ spreadsheet.js
@@ -19,7 +19,7 @@ class GoogleTable {
             this.sheet = this.doc.sheetsByIndex[0];
             this.rows = await this.sheet.getRows();
         } catch (error) {
-            logger.error(error);
+            logger.error(`Can't connect to Google Spreadsheet. Please, check spreadsheet ID: ${error}`);
         }
     }
 }
