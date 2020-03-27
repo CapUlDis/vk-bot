c3cdc6687bbcc0391b4e8852d9d275aaafd434c1
CapUlDis <capuldis@gmail.com>
2020-03-27 14:25:21 +0300

    Add test for error log in GoogleTable class in spreadsheet.

--- test/spreadsheet.test.js
+++ test/spreadsheet.test.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
+const sinon = require('sinon');
 const assert = require('chai').assert;
 const expect = require('chai').expect;
+
 const { GoogleTable } = require('../spreadsheet');
-require('dotenv').config()
+const logger = require('../logger');
 
 
 describe('Tests for class GoogleTable.', function() {
@@ -11,9 +14,18 @@ describe('Tests for class GoogleTable.', function() {
         await test_table.getDocInfo();
         assert.equal(test_table.sheet.title, 'График Дежурств');
     });
+
+    it('getDocInfo method throws error and correct log.', async () => {
+        const stub = sinon.stub(logger, 'error');
+        const test_table = new GoogleTable('foo');
+        await test_table.getDocInfo();
+        assert.isTrue(logger.error.calledOnce);
+        assert.isTrue(stub.args[0][0].includes("Can't connect to Google Spreadsheet."));
+        stub.restore();
+    });
 });
 
-    // it('getDocInfo method throws error and log.', async () => {
-    //     const test_table = new GoogleTable('foo');
-    //     await test_table.getDocInfo();
-    // })
+    
+
+
+
