1dd7a15e87fc4902ea647eb44fc01ae3bc26b39f
CapUlDisHome <54175504+CapUlDis@users.noreply.github.com>
2020-03-31 17:36:23 +0300

    Add / /g in replace.

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.command('дежурство', async (ctx) => {
             tableArray.push(rowObj);
         }
         let botAnswer = stringTable.create(tableArray);
-        botAnswer.replace(' ', '\u3000');
+        botAnswer.replace(/ /g, '\u3000');
         console.log(botAnswer);
         ctx.reply(botAnswer);
     } catch (error) {
