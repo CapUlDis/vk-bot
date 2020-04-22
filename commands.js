da6e72b62503700bdf2e22d1fc82f230087899c9
CapUlDisHome <54175504+CapUlDis@users.noreply.github.com>
2020-04-22 18:19:30 +0300

    Some nits in first command.

--- commands.js
+++ commands.js
@@ -9,21 +9,20 @@ const getCurrentDuties = async ctx => {
     try {
         const tableM3 = new GoogleTable({ sheetID: process.env.SPREADSHEET_ID, sheetIndex: process.env.SHEET_INDEX });
         await tableM3.getSheetRows(process.env.SHEET_INDEX);
-        if (tableM3.rows[0] != undefined) {
-            let today = moment();
-            for (let i = tableM3.rows.length - 1; i >= 0; i--) {
-                let dateUp = moment(tableM3.rows[i]['Период'], 'DD-MM-YY');
-                let weekBefore = moment(tableM3.rows[i]['Период'], 'DD-MM-YY').subtract(7, 'days');
-                if (today <= dateUp && today > weekBefore) {
-                    ctx.reply(`В срок до ${dateUp.format('L')} дежурят по кухне - ${tableM3.rows[i]['Кухня']}, по КВТ - ${tableM3.rows[i]['КВТ']}.`);
-                    break;
-                } else if (today > dateUp || i == 0) {
-                    ctx.reply(`На текущий период дежурств не запланировано. Заполните график по ссылке https://docs.google.com/spreadsheets/d/${process.env.SPREADSHEET_ID}`);
-                    break;
-                }
-            }
-        } else {
+        if (tableM3.rows[0] == undefined) {
             ctx.reply(`График дежурств пустой. Заполните график по ссылке https://docs.google.com/spreadsheets/d/${process.env.SPREADSHEET_ID}`);
+        }
+        let today = moment();
+        for (let i = tableM3.rows.length - 1; i >= 0; i--) {
+            let dateUp = moment(tableM3.rows[i]['Период'], 'DD-MM-YY');
+            let weekBefore = moment(tableM3.rows[i]['Период'], 'DD-MM-YY').subtract(7, 'days');
+            if (today <= dateUp && today > weekBefore) {
+                ctx.reply(`В срок до ${dateUp.format('L')} дежурят по кухне - ${tableM3.rows[i]['Кухня']}, по КВТ - ${tableM3.rows[i]['КВТ']}.`);
+                break;
+            } else if (today > dateUp || i == 0) {
+                ctx.reply(`На текущий период дежурств не запланировано. Заполните график по ссылке https://docs.google.com/spreadsheets/d/${process.env.SPREADSHEET_ID}`);
+                break;
+            }
         }
     } catch (error) {
         logger.error(error);
