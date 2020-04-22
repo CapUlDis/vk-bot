1db09ab3c432aac9d75c013ac7aab66f2c80e99e
CapUlDisHome <54175504+CapUlDis@users.noreply.github.com>
2020-04-22 12:38:42 +0300

    Change tableM3.rows[tableM3.rows.length - 1] to tableM3.rows[tableM3.rows.length - 1][Период] - that was huge mistake.

--- commands.js
+++ commands.js
@@ -47,7 +47,7 @@ const fillScheduleByLastDuties = async ctx => {
         logger.info(dutyList);
         if (dutyList.length % 2 == 0) {
             for (let i = 0; i <= dutyList.length - 1; i + 2) {
-                let newDutyDate = moment(tableM3.rows[tableM3.rows.length - 1], 'DD-MM-YY').add(7, 'days');
+                let newDutyDate = moment(tableM3.rows[tableM3.rows.length - 1]['Период'], 'DD-MM-YY').add(7, 'days');
                 logger.info(newDutyDate.format('L'));
                 tableM3.addOneRow({ Период: newDutyDate.format('L'), Кухня: dutyList[i + 1], КВТ: dutyList[i] });
                 tableM3.sheet.getRows();
@@ -55,7 +55,7 @@ const fillScheduleByLastDuties = async ctx => {
         } //else {
         //     dutyList.push(dutyList[0]);
         //     for (let i = 0; i <= dutyList.length - 1; i + 2) {
-        //         let newDutyDate = moment(tableM3.rows[tableM3.rows.length - 1], 'DD-MM-YY').add(7, 'days');
+        //         let newDutyDate = moment(tableM3.rows[tableM3.rows.length - 1]['Период'], 'DD-MM-YY').add(7, 'days');
         //         tableM3.addRow({ Период: newDutyDate.format('L'), Кухня: dutyList[i], КВТ: dutyList[i + 1] });
         //         tableM3.getSheetRows();
         //     }
