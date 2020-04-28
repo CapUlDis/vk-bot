51cb6abc1eb9371fabb9929c70001deb366a11b5
CapUlDisHome <54175504+CapUlDis@users.noreply.github.com>
2020-04-28 12:39:17 +0300

    Add button open google table and oneTime prop to keybord Duty.

--- index.js
+++ index.js
@@ -40,13 +40,25 @@ bot.command(/Дежурства/i, ctx => {
         Markup.button('Изменить текущих', 'negative')
       ],
       [
-        Markup.button('Автозаполнение графика')
+        Markup.button('Автозаполнение графика'),
+        Markup.button({
+          action: {
+            type: 'open_link',
+            link: 'https://google.com',
+            label: 'Открыть гугл-таблицу',
+            payload: JSON.stringify({
+              url: 'https://docs.google.com/spreadsheets/d/${process.env.SPREADSHEET_ID}',
+            }),
+          },
+          color: 'default',
+        })
       ],
       [
         Markup.button('Чек-лист кухни'),
         Markup.button('Чек-лист КВТ')
       ]
     ])
+    .oneTime()
   );
 });
 
