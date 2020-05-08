6a417769fc2e484c8a0a3f348e1885ed3d1c9c93
capuldis <capuldis@gmail.com>
2020-05-08 11:54:35 +0300

    Test another route.

--- index.js
+++ index.js
@@ -89,12 +89,12 @@ app.use(expressLogger);
 
 app.use(bodyParser.json());
 
-app.post('/', bot.webhookCallback);
+// app.post('/', bot.webhookCallback);
 
 app.post('/', function (req, res) {
   logger.info(req.body);
-  // bot.webhookCallback(req, res);
-  logger.info(res.body);
+  bot.webhookCallback(req, res);
+  logger.info(res);
 });
 
 app.listen(process.env.PORT || 3000);
