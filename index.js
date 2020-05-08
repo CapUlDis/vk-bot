038625468a5a3b97a58f90482aba450bf8e4990c
capuldis <capuldis@gmail.com>
2020-05-08 10:51:51 +0300

    Comment custom button, put custom button from docs.

--- index.js
+++ index.js
@@ -36,6 +36,17 @@ bot.command(/Дежурства/i, ctx => {
   try {
     ctx.reply('Выбери действие:', null, Markup
       .keyboard([
+        Markup.button({
+          action: {
+            type: 'open_link',
+            link: 'https://google.com',
+            label: 'Open Google',
+            payload: JSON.stringify({
+              url: 'https://google.com',
+            }),
+          },
+          color: 'default',
+        }),
         // [
         //   Markup.button('Показать текущих', 'positive'),
         //   Markup.button('Изменить текущих', 'negative')
@@ -43,17 +54,17 @@ bot.command(/Дежурства/i, ctx => {
         // [
         //   Markup.button('Автозаполнение графика'),
         // ],
-        Markup.button({
-          action: {
-            type: 'open_link',
-            link: `https://docs.google.com/spreadsheets/d/${process.env.SPREADSHEET_ID}`,
-            label: 'Открыть гугл-таблицу',
-            payload: JSON.stringify({
-              url: `https://docs.google.com/spreadsheets/d/${process.env.SPREADSHEET_ID}`,
-            }),
-          },
-          color: 'default',
-        })
+        // Markup.button({
+        //   action: {
+        //     type: 'open_link',
+        //     link: `https://docs.google.com/spreadsheets/d/${process.env.SPREADSHEET_ID}`,
+        //     label: 'Открыть гугл-таблицу',
+        //     payload: JSON.stringify({
+        //       url: `https://docs.google.com/spreadsheets/d/${process.env.SPREADSHEET_ID}`,
+        //     }),
+        //   },
+        //   color: 'default',
+        // })
         // [
         //   Markup.button('Чек-лист кухни'),
         //   Markup.button('Чек-лист КВТ')
